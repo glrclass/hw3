70ff2b317e3e7296f3db93b30bf27be3a2d4f31e
G R <gabi.romagnoli@kellogg.northwestern.edu>
2025-02-26 04:01:45 +0000

    created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
+
+  get("/places", {:controller => "places", :action => "index"})
+  get("/entries"), {:controller => "places", :action => "index"})
+
+  # will do actions for new, create and show. also need resources ???
 end
